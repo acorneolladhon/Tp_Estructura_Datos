8cfe68cf215563f784a850c0a1d8e7334842e00d
´acorneolladhon´ <acorneolladhon@itba.edu.ar>
2023-04-04 18:19:13 -0300

    :)

--- tp.py
+++ tp.py
@@ -12,11 +12,6 @@ print("Hola ")
 
 #que onda
 
-<<<<<<< HEAD
 print("hola soy jose")
-=======
-<<<<<<< HEAD
-#river
-=======
->>>>>>> e9c62f4605d5b42c65d450f97851cd1c30bcb307
->>>>>>> f8cefb0e680dec29bcc18a12afc972a3c927933a
+
+# :)
