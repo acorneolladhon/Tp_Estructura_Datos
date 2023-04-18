4b57b3b9b9f7b76edb33c83fb6036d88ba2e6a81
josegeocrak <jgeoghegan@itba.edu.ar>
2023-04-18 19:22:44 -0300

    clase 18/04

--- tp.py
+++ tp.py
@@ -1,17 +1,73 @@
-print("Hola mundo")
-print("Hola ")
-#clase balneario
-#clase persona
-    #clase empleado
-    #clase cliente
-   
-#clase restaurant
-#clase carpa
-#HOLA
+import numpy as np 
 
+class Carpa():
+    def __init__(self):
+        estado=None
 
-#que ondtdhdthdtha
+class Sombrilla():
+    def __init__(self):
+        estado=None
 
-print("hola soy jose")
+class Balneario():
+    def __init__(self, nombre):         
+        self.nombre=nombre
+        self.dicemp=dict()
+        carpas=[[Carpa() for i in range(6)] for i in range(4)]
+        self.m_carpas=np.array(carpas)
+        sombris=[[Sombrilla() for i in range(5)] for i in range(3)]
+        self.m_sombrillas=np.array(sombris)
 
-# :)
+    def validar_contraseña(self):
+        pass
+    #también podemos hacer un archivo csv con usuarios y contraseñas
+    #en el diccionario de empleados, podemos guardar a cada empleado con su contraseña en la KEY (y para buscarlo, lo hacemos a través de la función)
+
+    def leer_archivos(self, info_emps, info_matrices):
+        pass
+    #con esta función, al iniciar el programa desde el main, buscamos toda la información del balneario en los archivos que llamamos
+
+    def cargar_archivos(self, info_emps, info_matrices):
+        pass
+    #con esta función, al cerrar el programa, guardamos toda la info que recolectamos durante esa utilización del programa 
+
+class Reserva():
+    def __init__(self, num_reserva, tiempo_estadia, tipo_reserva):
+        self.numreserva=num_reserva
+        self.tiempo_estadia=tiempo_estadia #en días
+        self.precio=0  # el precio lo calcula el empleado al crear la reserva
+        self.tipo_reserva=tipo_reserva
+
+class Empleado():
+    def __init__(self, codemp, dni):
+        self.codemp=codemp
+
+    
+    def registrar_cliente():
+        pass
+    #acá podemos pedirle los datos al cliente que quiere reservar, preguntarle el tiempo y registrarlo en la lista de clientes (?)
+    #también se podría pedir desde el menú --> para no complicar el menú quizas es más facil desde acá 
+
+    def asignar_reserva(matriz, tipo_reserva):
+        pass
+    #según si es sombrilla o carpa, revisamos las matrices para ver si tienen lugares disponibles, y en base a eso asignamos una posicion
+
+    def calcular_precio(): 
+        pass
+    #este precio despues se lo pasamos a la reserva que creemos
+
+    def modificar_estadia():
+        pass
+    #viene el cliente y pide modificar su tiempo de estadia
+
+    def revisar_matrices(self):
+        pass
+    #chequeamos las fechas al final del programa
+    #CHEQUEAMOS LAS MATRICES (ejemplo si ya expiraron las reservas)
+    
+    def ver_matriz(self):
+        pass
+    #visualice en pantalla la matriz del balneario
+
+    
+#cada vez 
+#entro con mi usuario --> una
