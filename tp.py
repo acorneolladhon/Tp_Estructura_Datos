7e50ad01242db4014e9a20c67727d6a426716137
manuerc <mercolinikenny@itba.edu.ar>
2023-04-30 16:02:29 -0300

    h

---
+++ tp.py
@@ -0,0 +1,17 @@
+print("Hola mundo")
+print("Hola ")
+#clase balneario
+#clase persona
+    #clase empleado
+    #clase cliente
+   
+#clase restaurant
+#clase carpa
+#HOLA
+
+
+#que ondtdhdthdtha
+
+print("hola soy jose")
+
+# :)
